c25375593fabb8ae1ff03d762e7a22d3986cc6f6
Aman Gupta <4programmimg@gmail.com>
2023-07-26 17:24:41 +0530

    run pod initial scripts

---
+++ run0.sh
@@ -0,0 +1,21 @@
+# use bash filename.sh to execute
+# cuda v117 then pytorch/torch = 1.13.1 and xformers v0.0.19
+# can install torch 2.0.1/0 and xformers 0.0.20 but idk cuda version -U=--upgrade
+# while (n<1): in relauncher.py, then copy config.json for settings
+# pip3 uninstall <package-name>
+# using pip3 !pip installer as pip3 isfor py_V3.x.x
+echo make sure to update requirements file then
+echo "cd /workspace/stable-diffusion-webui, pip3 install -r requirements.txt"
+python --version
+pip3 -V
+pip3 install --upgrade pip3
+# python3 -m pip install --upgrade pip
+## echo "installing torch 2.0.0"
+## pip3 install torch==2.0.0 torchvision torchaudio torchsde --index-url https://download.pytorch.org/whl/cu117 --upgrade(will --upgrade in behind will work)
+echo "installing xformers latest requires pytorch2.0.0"
+#pip3 install xformers==0.0.19
+# pip3 install -U xformers
+python -m xformers.info
+python -m xformers.info output
+pip3 list
+echo "<===== DONE RUN0.SH =====>"
---
+++ run2.sh
@@ -0,0 +1,6 @@
+#kill port and manual restart for terminal progress bar
+echo kill port and manual restart for terminal SD progress bar
+fuser -k 3000/tcp
+python relauncher.py
+echo "scan for civitai images, use extension and update them and VAE settings"
+echo "<===== DONE RUN2.SH =====>"
